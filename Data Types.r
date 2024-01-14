de4843f420dfeb37ad132a97cbe67ead3e91b4be
Pankaj Sharma <118182606+PankajSharma3@users.noreply.github.com>
2024-01-14 23:17:03 +0530

    Create Data Types.r

---
+++ Data Types.r
@@ -0,0 +1,12 @@
+name = "Pankaj Sharma"
+print(class(name))  #Character
+a = TRUE
+print(class(a)) #Logical
+b = 12343
+print(class(b)) #Numeric
+c = 12345L
+print(class(c)) #Integer
+d = 2+3i
+print(class(d)) #Complex
+e = charToRaw("Hello World")
+print(class(e)) #Raw
