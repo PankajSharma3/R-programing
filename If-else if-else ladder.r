11b09d4e4005c58eb43f7b0c253fa4a431c2c5f0
Pankaj Sharma <118182606+PankajSharma3@users.noreply.github.com>
2024-01-15 18:43:13 +0530

    Create If-else if-else ladder.r

---
+++ If-else if-else ladder.r
@@ -0,0 +1,10 @@
+age = 34;
+if(age>90 && age<=100){
+    print("O grade")
+}else if (age>=80 && age<90) {
+   print("A+ grade")
+}else if(age>=70 && age<80) {
+   print("A grade")
+}else {
+   print("Failed")
+}
