2a680ce41244794ca24c3f182ed1d98a26d53bc9
Pankaj Sharma <118182606+PankajSharma3@users.noreply.github.com>
2024-01-15 22:19:29 +0530

    Create vector.r

---
+++ Vectors/vector.r
@@ -0,0 +1,13 @@
+# Vectors
+# a <- 1:-10
+# print(a)
+# b <- seq(1, 5, 0.5)
+# print(b)
+
+
+b = c(3.43,45.43,3.5)  #Atomic Vectors
+b = as.integer(b)
+print(b)
+print(class(b))
+b = as.character(b)
+print(b)
