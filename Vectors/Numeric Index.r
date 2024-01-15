aed46e8d5042df286ef726ecf7439e5fc5d3891f
Pankaj Sharma <118182606+PankajSharma3@users.noreply.github.com>
2024-01-15 22:31:16 +0530

    Create Numeric Index.r

---
+++ Vectors/Numeric Index.r
@@ -0,0 +1,6 @@
+a = c(1,2,3,4,5)
+print(a[0])
+print(a[1])
+print(a[-2])  #It will return the full vector except the abs(-2) = 2 that means the value at index 2
+print(a[11]) #NA
+print(a[-11]) #Gives the output as the full vector
