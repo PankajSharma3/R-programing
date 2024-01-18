973c193221f4c12e7a40e06b159418d02593aff7
Pankaj Sharma <118182606+PankajSharma3@users.noreply.github.com>
2024-01-18 16:45:35 +0530

    Create Data Frames.r

---
+++ Data Frames.r
@@ -0,0 +1,26 @@
+a = data.frame(
+    id = c(1:5),
+    name = c("Amit","Anshu","Ankit","Aman","Anil"),
+    salary = c(10000,54000,34000,53333,54000)
+)
+print(a)
+
+
+#Accessing particular column from the data frame
+b = data.frame(a$id,a$name)
+print(b)
+
+
+#Accessing particular row from the data frame
+c = a[1,]
+print(c)
+
+
+#Accessing more than 1 consecutive rows from a data frame
+d = a[3:4,]
+print(d)
+
+
+#Accessing elements from the row corresponding specific column
+e = a[c(2,3),c(2,3)]
+print(e)
