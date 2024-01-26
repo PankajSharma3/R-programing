a37a1769a4d8e5b88682a1456f1ddd0ee0bde59b
Pankaj Sharma <118182606+PankajSharma3@users.noreply.github.com>
2024-01-26 19:12:02 +0530

    Create Inbuild function.r

---
+++ Inbuild function.r
@@ -0,0 +1,11 @@
+#print list of Uppercase letters
+print(LETTERS)
+
+#print list of Lowercase letters
+print(letters)
+
+#print 3 letters of abb. of english months
+print(month.abb)
+
+#print numerical value of constants pi
+print(pi)
