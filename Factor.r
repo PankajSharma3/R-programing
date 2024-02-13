5b1b62936f0ae2140e02ecc27ba110fd7b9a1465
Pankaj Sharma <118182606+PankajSharma3@users.noreply.github.com>
2024-02-13 10:57:33 +0530

    Update Factor.r

--- Factor.r
+++ Factor.r
@@ -2,3 +2,14 @@
 a = c('green','red','yellow','red','red','green')
 b = factor(a)
 print(levels(b))
+
+
+
+# Factor in R programming
+a = c(1,23,3,34,434,33,43,42,345);
+print(a)
+print(is.factor(a))
+b = factor(a) # Factor sort the data
+print(b)
+print(is.factor(b)) # Is vector returns the 
+print(b[-2]) # print the value of the factor where the given index doesnot print
